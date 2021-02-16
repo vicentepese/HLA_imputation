6e5da158454a36cca4aa2456d0b2b7c093791efb
vicentepese <sempereperisvicente@gmail.com>
2021-02-16 13:23:59 -0800

    Comments

--- HLA_imputation.R
+++ HLA_imputation.R
@@ -45,6 +45,7 @@ option_list = list(
               help = "Ethnicity name", metavar = "character")
 ); 
  
+# Parse arguments
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
@@ -54,8 +55,6 @@ if (is.null(opt$file)){
   stop("Please provide a file through --file argument.", call.=FALSE)
 }
 
-
-
 # Check that passes locus are acceptable
 if (opt$locus %notin% c("A","B","C","DPB1","DQA1","DQB1","DRB1","DRB3", "DRB4", "DRB5")){
   stop("Loci provided must be any of the following options: \r
@@ -64,7 +63,7 @@ if (opt$locus %notin% c("A","B","C","DPB1","DQA1","DQB1","DRB1","DRB3", "DRB4", 
 
 ########## HLA IMPUTATION ############
 
-# Load pre-fit model and comvert to hlaMODEL based on eth option
+# If no ethnicity, load default model, otherwise load ethnicity specific model
 if (is.null(opt$eth)){
   
   # Load model
@@ -73,23 +72,22 @@ if (is.null(opt$eth)){
   drb4 <- get(load(settings$models$DRB4))
   drb5 <- get(load(settings$models$DRB5))
   
-  # Merge models 
+  # Merge models with DRB3, DRB4 and DRB5
   model.list[["DRB3"]] <- drb3
   model.list[["DRB4"]] <- drb4
   model.list[["DRB5"]] <- drb5
   
-   # Import file
+   # Import PLINK binary file
   gname <- opt$file
   yourgeno <- hlaBED2Geno(bed.fn=paste(gname, ".bed", sep = ''), fam.fn=paste(gname, ".fam", sep='')
                           , bim.fn=paste(gname, ".bim", sep=''), assembly = 'hg19')
     
-  # Summary
+  # Summary of imnput file
   summary(yourgeno)
-
 
 } else{
   
-  # Load model based on eth 
+  # Load model based on ethnicity
   switch(opt$eth, 
          "EUR"= {model.list <- get(load(settings$models$EURmodel))},
          "SAS"= {model.list <- get(load(settings$models$ASImodel))},
@@ -102,7 +100,7 @@ if (is.null(opt$eth)){
   eth.df <- read.table(file = settings$ethnicity, header = TRUE, sep = ",")
   eth.df$sample.id <- paste0(eth.df$FID, rep("-", nrow(eth.df)), eth.df$IID)
   
-  # Import file
+  # Import PLINK binary file
   gname <- opt$file
   yourgeno <- hlaBED2Geno(bed.fn=paste(gname, ".bed", sep = ''), fam.fn=paste(gname, ".fam", sep='')
                           , bim.fn=paste(gname, ".bim", sep=''), assembly = 'hg19')
@@ -113,32 +111,32 @@ if (is.null(opt$eth)){
   yourgeno$sample.id <- yourgeno$sample.id[sample.idx]
   yourgeno$genotype <- yourgeno$genotype[,sample.idx]
   
-  # Summary
+  # Summary of input file
   summary(yourgeno)
   
 }
 
-# Make cluster
+# Make cluster for parallelization
 cl <- makeCluster(10)
 
-# Filter model
+# Filter model based on input locus
 model <- model.list[[opt$locus]]
 
-# Make predictions
+# Make predictions with HIBAG
 model.hla <- hlaModelFromObj(model)
 summary(model.hla)
 pred.guess <- predict(model.hla, yourgeno, type="response+prob", cl=cl, match.type="Position")
 
-# Import fam 
+# Import .fam file 
 fam.file <- read.table(file = paste0(gname, ".fam"), sep = " ", header = FALSE)
 sample.id <- paste(fam.file$V1, fam.file$V2, sep = "-")
 
-# Create sample.id and create FID and IID
+# Modify sample.id and create FID and IID
 pred.guess$value$sample.id <- sample.id
 pred.guess$value$FID <- pred.guess$value$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% .[1]) %>% unlist()
 pred.guess$value$IID <- pred.guess$value$sample.id %>% lapply(function(x) strsplit(x,"-") %>% unlist() %>% tail(n=1)) %>% unlist()
 
-# Save
+# Save file as .RData
 prefix <- opt$file %>% strsplit("/") %>% unlist() %>% tail(n=1)
 if (is.null(opt$eth)){
   save(pred.guess, file = paste0(opt$out, prefix,'_HLA_', opt$locus, "_", opt$eth, '.RData'))
